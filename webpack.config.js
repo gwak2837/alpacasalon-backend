d0531ca30c54396a5f7c408e1c0d443a626b9982
Taeuk Gwak <gwak2837@kakao.com>
2021-12-28 15:47:13 +0900

    False minimize

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
     path: resolve(__dirname, 'dist'),
   },
   optimization: {
-    minimize: true,
+    minimize: false,
     minimizer: [
       new TerserPlugin({
         parallel: true,
