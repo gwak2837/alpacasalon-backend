857bd4aeadf37cc6d334231ea065da8bbc8afc43
Taeuk Gwak <gwak2837@kakao.com>
2022-01-06 15:10:37 +0900

    Fix comment without profile image

--- src/graphql/comment/ORM.ts
+++ src/graphql/comment/ORM.ts
@@ -1,53 +1,71 @@
-import { DeepPartial } from '../../utils/types'
 import { Comment } from '../generated/graphql'
 
 export function commentORM(rows: Record<string, any>[]) {
   if (rows.length === 0) return null
 
-  const comments: DeepPartial<Comment>[] = []
+  const comments = []
 
   for (const row of rows) {
-    let comment = comments.find((comment) => comment.id === row.id)
+    let existingComment = comments.find((comment) => comment.id === row.id)
 
-    if (!comment) {
-      comment = {
+    if (existingComment) {
+      existingComment.subcomments.push({
+        id: row.subcomments__id,
+        creationTime: row.subcomments__creation_time,
+        modificationTime: row.subcomments__modification_time,
+        contents: row.subcomments__contents,
+        isLiked: row.subcomments__is_liked,
+        isModified:
+          row.subcomments__creation_time &&
+          row.subcomments__modification_time &&
+          row.subcomments__creation_time !== row.subcomments__modification_time,
+        likedCount: row.subcomments__liked_count,
+        user: {
+          id: row.subcomments__user__id,
+          nickname: row.subcomments__user__nickname,
+          imageUrl: row.subcomments__user__image_url,
+        },
+      })
+    } else {
+      const newComment = {
         id: row.id,
         creationTime: row.creation_time,
         modificationTime: row.modification_time,
         contents: row.contents,
+        likedCount: row.liked_count,
         isLiked: Boolean(row.is_liked),
         isModified:
           row.creation_time && row.modification_time && row.creation_time !== row.modification_time,
-        likedCount: row.liked_count,
         user: {
           id: row.user__id,
           nickname: row.user__nickname,
-          imageUrl: row.user__imageUrl,
+          imageUrl: row.user__image_url,
         },
+        subcomments: [] as any[],
       }
-      comments.push(comment)
+
+      if (row.subcomments__id) {
+        newComment.subcomments.push({
+          id: row.subcomments__id,
+          creationTime: row.subcomments__creation_time,
+          modificationTime: row.subcomments__modification_time,
+          contents: row.subcomments__contents,
+          isLiked: row.subcomments__is_liked,
+          isModified:
+            row.subcomments__creation_time &&
+            row.subcomments__modification_time &&
+            row.subcomments__creation_time !== row.subcomments__modification_time,
+          likedCount: row.subcomments__liked_count,
+          user: {
+            id: row.subcomments__user__id,
+            nickname: row.subcomments__user__nickname,
+            imageUrl: row.subcomments__user__image_url,
+          },
+        })
+      }
+
+      comments.push(newComment)
     }
-
-    if (row.subcomments__id === null) continue
-    if (!comment.subcomments) comment.subcomments = []
-
-    comment.subcomments.push({
-      id: row.subcomments__id,
-      creationTime: row.subcomments__creation_time,
-      modificationTime: row.subcomments__modification_time,
-      contents: row.subcomments__contents,
-      isLiked: row.subcomments__is_liked,
-      isModified:
-        row.subcomments__creation_time &&
-        row.subcomments__modification_time &&
-        row.subcomments__creation_time !== row.subcomments__modification_time,
-      likedCount: row.subcomments__liked_count,
-      user: {
-        id: row.subcomments__user__id,
-        nickname: row.subcomments__user__nickname,
-        imageUrl: row.subcomments__user__image_url,
-      },
-    })
   }
 
   return comments as Comment[]
--- src/graphql/comment/Object.ts
+++ src/graphql/comment/Object.ts
@@ -4,10 +4,16 @@ export const Comment: CommentResolvers = {
   isLiked: ({ isLiked }) => {
     return !!isLiked
   },
+
   isModified: ({ isModified }) => {
     return !!isModified
   },
+
   user: ({ user }) => {
     return user?.id ? user : null
+  },
+
+  subcomments: ({ subcomments }) => {
+    return subcomments && subcomments.length > 0 ? subcomments : null
   },
 }
