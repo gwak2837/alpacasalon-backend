c23fa3b89f60653c8b6e85f9bc5277d6cd4b0494
Taeuk Gwak <gwak2837@kakao.com>
2022-01-11 12:57:03 +0900

    Update postsByGroup.sql

--- src/graphql/post/sql/postsByGroup.sql
+++ src/graphql/post/sql/postsByGroup.sql
@@ -6,7 +6,8 @@ SELECT post.id,
   post.image_urls,
   COUNT("comment".id) AS comment_count,
   "user".id AS user__id,
-  "user".nickname AS user__nickname
+  "user".nickname AS user__nickname,
+  "user".image_url AS user__image_url
 FROM post
   LEFT JOIN "user" ON "user".id = post.user_id
   LEFT JOIN "comment" ON "comment".post_id = post.id
