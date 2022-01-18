746c1e5e45a5a8f1bfbb411f116b17cf395ac4a7
hyo-jin-jeong <55984573+hyo-jin-jeong@users.noreply.github.com>
2022-01-18 16:57:31 +0900

    Add post api group check

--- src/graphql/comment/sql/doesUserJoinGroup.sql
+++ src/graphql/comment/sql/doesUserJoinGroup.sql
@@ -1,5 +1,8 @@
-SELECT user_x_group.user_id
-FROM post
-  JOIN "group" ON "group".id = post.group_id
-  JOIN user_x_group ON user_x_group.group_id = "group".id
-WHERE post.id = $1 -- user_id가 여러개 나옴
+SELECT user_id
+FROM user_x_group
+WHERE user_id = $1
+  AND group_id = (
+    SELECT group_id
+    FROM post
+    WHERE post.id = $2
+  );
