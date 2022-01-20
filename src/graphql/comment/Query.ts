5bf3db5d8d4473d14edb366842f2c56a357d76b1
hyo-jin-jeong <55984573+hyo-jin-jeong@users.noreply.github.com>
2022-01-20 14:56:02 +0900

    Fix comment gorup check bug

--- src/graphql/comment/Query.ts
+++ src/graphql/comment/Query.ts
@@ -5,15 +5,21 @@ import { poolQuery } from '../../database/postgres'
 import { QueryResolvers } from '../generated/graphql'
 import { commentORM } from './ORM'
 import commentsByPost from './sql/commentsByPost.sql'
+import getGroupIdOfPost from './sql/getGroupIdOfPost.sql'
 import doesUserJoinGroup from './sql/doesUserJoinGroup.sql'
 
 export const Query: QueryResolvers<ApolloContext> = {
   commentsByPost: async (_, { postId }, { userId }) => {
     if (!userId) throw new AuthenticationError('로그인 후 시도해주세요.')
 
-    const { rowCount } = await poolQuery(doesUserJoinGroup, [userId, postId])
+    const { rows } = await poolQuery(getGroupIdOfPost, [postId])
+    const groupId = rows[0].group_id
 
-    if (rowCount === 0) throw new ForbiddenError('해당 그룹에 속해 있지 않습니다.')
+    if (groupId) {
+      const { rowCount } = await poolQuery(doesUserJoinGroup, [userId, groupId])
+
+      if (rowCount === 0) throw new ForbiddenError('해당 그룹에 속해 있지 않습니다.')
+    }
 
     const { rows: rows2 } = await poolQuery(commentsByPost, [postId, userId])
 
--- src/graphql/comment/sql/doesUserJoinGroup.sql
+++ src/graphql/comment/sql/doesUserJoinGroup.sql
@@ -1,8 +1,4 @@
 SELECT user_id
 FROM user_x_group
 WHERE user_id = $1
-  AND group_id = (
-    SELECT group_id
-    FROM post
-    WHERE post.id = $2
-  );
+  AND group_id = $2;
